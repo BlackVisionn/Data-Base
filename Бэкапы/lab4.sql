abe214bfe0527809c3872dc1ee14327f15f56068
BlackVision <19106267@vstu.ru>
2021-05-12 00:58:27 +0300

    Добавлены запросы с DELETE
    DELETE в разных таблицах, с WHERE

--- Бэкапы/lab4.sql
+++ Бэкапы/lab4.sql
@@ -27,3 +27,19 @@ WHERE department_name = "Бухгалтерия";
 UPDATE position_to_organization
 SET salary = salary + 2000
 WHERE salary > 45000;
+
+#Удалить сотрудников у которых опыт работы 1 месяц
+DELETE FROM employees WHERE work_experience = "1 месяц";
+
+#Удалить сотрудников у которых среднее образование
+DELETE FROM diploma_of_education WHERE education = "среднее";
+
+#Удалить сотрудников у которых Учебный отпуск
+DELETE FROM vacation WHERE type_of_vacation = "Учебный";
+
+#Удалить сотрудника у которого табельный номер 85
+DELETE FROM employees WHERE personnel_number = "85";
+
+#Удалить сотрудников которые родились 12.05.1998
+DELETE FROM personal_data WHERE birthday = "1998-05-12";
+
