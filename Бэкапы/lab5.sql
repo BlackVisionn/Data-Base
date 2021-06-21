5771759a1c3946e6c597bef4c52eb579c06a4e03
BlackVisionn <19106267@vstu.ru>
2021-06-22 00:39:13 +0300

    Добавлены еще индексы

--- Бэкапы/lab5.sql
+++ Бэкапы/lab5.sql
@@ -14,11 +14,25 @@ WHERE personnel_number < 100;
 
 CREATE INDEX idx_employees_personnel_number ON employees(personnel_number);
 
-#Вывод ФИО сотрудников которые являюст женщинами
-SELECT vacation.type_of_vacation
+#Вывести тип отпуска сотрудников у которых дней отпуска меньше 5
+EXPLAIN SELECT vacation.type_of_vacation
 FROM vacation
-WHERE number_of_vacation_days < 10;
+WHERE number_of_vacation_days < 5;
 
 CREATE INDEX idx_vacation_number_of_vacation_days ON vacation(number_of_vacation_days);
 
-DROP INDEX idx_contract_date_of_receipt ON contract;
+#Вывести имена сотрудников которые живут по определенному адресу
+EXPLAIN SELECT employees.full_name
+FROM employees
+WHERE home_address LIKE '%Street%';
+
+CREATE INDEX idx_employees_home_address ON employees(home_address);
+
+#Вывести название льгот сотрудника номер документа которых равен 709 или меньше 50
+EXPLAIN SELECT social_benefits.name_of_benefits
+FROM social_benefits
+WHERE document_number = 709 OR document_number < 50;
+
+CREATE INDEX idx_social_benefits_document_number ON social_benefits(document_number);
+
+DROP INDEX idx_social_benefits_document_number ON social_benefits;
